529743b01a76c78e35b04bcb75fc9ef9b059ca81
kimhakheng <hengkimhak18@kit.edu.kh>
2023-07-20 19:32:34 +0700

    fourth commit

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-8-puzzle/',
+    assetsPublicPath: './vue-8-puzzle/',
 
     /**
      * Source Maps
